aa3f29457942b6cf96402aa6b58677a78f42b652
raresmaier234 <raresmaier123@gmail.com>
2026-02-15 16:10:55 +0200

    changes

--- backend/booking-server.js
+++ backend/booking-server.js
@@ -27,6 +27,12 @@ const ensurePbAdminAuth = async () => {
 };
 
 router.get('/booking-availability', async (req, res) => {
+    // Prevent caching of availability data
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    res.set('Surrogate-Control', 'no-store');
+
     try {
         await ensurePbAdminAuth();
 
