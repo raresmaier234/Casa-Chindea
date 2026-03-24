6dbd1814d8a4bd05a9da0f2fde4710ac66bb3660
raresmaier234 <raresmaier123@gmail.com>
2026-03-24 20:23:30 +0200

    fix: use correct PocketBase Field() format for payment migration

--- api/pb_migrations/1774700000_add_payment_fields_to_prices.js
+++ api/pb_migrations/1774700000_add_payment_fields_to_prices.js
@@ -3,38 +3,42 @@ migrate((app) => {
   const collection = app.findCollectionByNameOrId("prices")
 
   // Add paymentMode field (select: none, full, deposit)
-  collection.fields.push({
-    "hidden": false,
+  collection.fields.addAt(collection.fields.length, new Field({
+    "system": false,
     "id": "paymentmode_field",
     "name": "paymentMode",
     "type": "select",
     "required": false,
     "presentable": false,
-    "system": false,
-    "values": ["none", "full", "deposit"],
-    "maxSelect": 1
-  })
+    "unique": false,
+    "options": {
+      "values": ["none", "full", "deposit"],
+      "maxSelect": 1
+    }
+  }))
 
   // Add depositPercent field
-  collection.fields.push({
-    "hidden": false,
+  collection.fields.addAt(collection.fields.length, new Field({
+    "system": false,
     "id": "depositpercent_field",
     "name": "depositPercent",
     "type": "number",
     "required": false,
     "presentable": false,
-    "system": false,
-    "min": 0,
-    "max": 100,
-    "onlyInt": true
-  })
+    "unique": false,
+    "options": {
+      "min": 0,
+      "max": 100,
+      "noDecimal": true
+    }
+  }))
 
   return app.save(collection)
 }, (app) => {
   const collection = app.findCollectionByNameOrId("prices")
 
-  collection.fields = collection.fields.filter(f => f.name !== 'paymentMode' && f.name !== 'depositPercent')
+  collection.fields.removeById("paymentmode_field")
+  collection.fields.removeById("depositpercent_field")
 
   return app.save(collection)
 })
-
